2de0b86af2140ac898cf7fde95e11c18d0969dbf
KING <2419688248@qq.com>
2019-10-30 20:46:22 +0800

    second commit

--- music/webpack.config.js
+++ music/webpack.config.js
@@ -1,22 +1,57 @@
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
-const configMode = (mode) => {
-	return require(`./build-utils/webpack-${mode}.js`);
-}
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = ({mode}) => {
+module.exports = ({ mode }) => {
 	return {
 		mode,
+		devtool: "eval-source-map",
 		entry: "./src/index.js",
 		output: {
 			path: __dirname + "/dist",
 			filename: "index.js"
 		},
+		module: {
+			rules: [{
+				test: /\.scss/,
+				use: [{
+					loader: MiniCssExtractPlugin.loader
+				}, "css-loader", "sass-loader"]
+			}, {
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+						plugins: ["@babel/plugin-syntax-dynamic-import"]
+					}
+				}]
+			}, {
+				test: /\.jpe?g$/,
+				use: [{
+					loader: "url-loader",
+					options: {
+						limit: 5000  //单位是b
+					}
+				}]
+			},
+			{
+				test: /\.mp3$/,
+				use: [{
+					loader: "url-loader",
+					options: {
+						limit: 5000  //单位是b
+					}
+				}]
+			}]
+		},
 		plugins: [
 			new htmlWebpackPlugin({
 				template: "./index_template.html",
 			}),
-			new webpack.ProgressPlugin()
+			new webpack.ProgressPlugin(),
+			new MiniCssExtractPlugin()
 		]
 	}
 }
